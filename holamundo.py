729e7a2e484f0ca3584e2ef6e0f4fd306fc029b1
Eduardo Grados <eduardogrados2@gmail.com>
2022-12-21 22:43:26 -0500

    segundo commit

--- holamundo.py
+++ holamundo.py
@@ -1,1 +1,4 @@
 print ("holamundo")
+print("sfsdssf")
+print("sesta es una prueba")
+print("a ver si funciona")
---
+++ my_mod.py
@@ -0,0 +1,8 @@
+some_numbers = [1, 2, 3]
+a = 20
+
+def add(x, y):
+    return x+y
+
+class Something:
+    pass
